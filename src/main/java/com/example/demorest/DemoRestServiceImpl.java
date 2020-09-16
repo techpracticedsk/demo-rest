338824316321efb9c0b3036fd2fd3e7dadfba450
subhroy <subhasis.it@gmail.com>
2020-09-16 23:53:32 +0530

    Spring boot practise project

--- src/main/java/com/example/demorest/DemoRestServiceImpl.java
+++ src/main/java/com/example/demorest/DemoRestServiceImpl.java
@@ -5,13 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public class DemoRestServiceImpl implements DemoRestService {
 
-
     @Autowired
     UserRepository userRepoInterface;
 
     @GetMapping("/allusers")
     public void getAllUser() {
-        System.out.println("I m inside ..get all users...");
+        System.out.println("I m inside...get all users...");
         Iterable itr = userRepoInterface.findAll();
     }
 }
