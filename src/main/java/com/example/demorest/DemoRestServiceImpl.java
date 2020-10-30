7cd2a24a9f026a8323d63f2589e275166288de44
subhroy <subhasis.it@gmail.com>
2020-10-30 19:02:58 +0530

    Removed unused import

--- src/main/java/com/example/demorest/DemoRestServiceImpl.java
+++ src/main/java/com/example/demorest/DemoRestServiceImpl.java
@@ -2,11 +2,8 @@ package com.example.demorest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.List;
 
 @Service
